3b27b9a9e77d1aa19f61d47c44d776398441e1ec
RatnambarBaghel <ratnambarbaghel14@gmail.com>
2026-06-19 21:59:10 +0530

    Time: 0 ms (100%), Space: 19.3 MB (68.12%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n<=0:
+            return False
+        primes =[2,3,5]
+        for i in primes:
+            while n%i ==0:
+                n /=i
+        return n==1
