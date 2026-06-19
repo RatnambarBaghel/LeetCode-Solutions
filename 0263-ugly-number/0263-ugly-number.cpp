27bbba8bc996ee2b9ee12cf92afea41dad37d33e
RatnambarBaghel <ratnambarbaghel14@gmail.com>
2026-06-19 22:00:50 +0530

    Time: 0 ms (100%), Space: 8 MB (7.82%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n<=0) return false;
+        int primes[]={2,3,5};
+        for(int i: primes){
+            while(n%i ==0){
+                n /=i;
+            }
+        }
+        return n==1;
+    }
+};
