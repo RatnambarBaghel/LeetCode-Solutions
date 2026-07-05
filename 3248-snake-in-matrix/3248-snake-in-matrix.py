62366b6e4af7dc038b4c3d9546d6318d90be591a
RatnambarBaghel <ratnambarbaghel14@gmail.com>
2026-07-05 18:18:53 +0530

    Time: 0 ms (100%), Space: 19.3 MB (27.76%) - LeetHub

---
+++ 3248-snake-in-matrix/3248-snake-in-matrix.py
@@ -0,0 +1,14 @@
+class Solution:
+    def finalPositionOfSnake(self, n: int, commands: List[str]) -> int:
+        ans =0
+        for command in commands:
+            if(command == "RIGHT"):
+                ans +=1
+            elif(command == "DOWN"):
+                 ans +=n
+            elif(command == "UP"):
+                 ans -=n
+            elif(command == "LEFT"):
+                 ans -=1
+        
+        return ans;
