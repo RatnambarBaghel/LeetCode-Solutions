f15d548ac8da4e4caa069a8a74b2fcb3c38bf679
RatnambarBaghel <ratnambarbaghel14@gmail.com>
2026-07-06 17:12:33 +0530

    Time: 19 ms (13.61%), Space: 48 MB (17.47%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -2,20 +2,19 @@ class Solution {
     public int[] findErrorNums(int[] nums) {
         int n = nums.length;
         int expectedSum = (n *(n+1))/2;
-        int prev =0;
-        int fix =0,sum =0;
-        int[] ans = new int[2];
-        Arrays.sort(nums);
+        int[]  ans = new int[2];
+        HashMap<Integer,Integer> map = new HashMap<>();
         for(int i=0;i<n;i++){
-            if(prev == nums[i]){
-                fix = prev;
-                continue;
-            }
-            sum += nums[i];
-            prev = nums[i];
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-        ans[0] = fix;
-        ans[1] = expectedSum - sum;
+        int sum =0;
+        int dup = 0;
+        for(int key: map.keySet()){
+            sum += key;
+            if(map.get(key) == 2) dup = key;
+        }
+        ans[0] = dup;
+        ans[1] = expectedSum -sum;
         return ans;
     }
 }
