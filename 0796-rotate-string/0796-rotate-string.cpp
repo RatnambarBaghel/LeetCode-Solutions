662e8a1ce769070cf503ad802f18bf5ca44f48dc
RatnambarBaghel <ratnambarbaghel14@gmail.com>
2026-07-10 16:23:18 +0530

    Time: 0 ms (100%), Space: 7.9 MB (99.18%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        int n = s.size();
+        for(int i=0;i<n;i++){
+            int temp = s[0];
+            for(int i=1;i<n;i++){
+                s[i-1] = s[i];
+            }
+            s[n-1] = temp;
+            if(s == goal) return true;
+        }
+        return false;
+    }
+};
