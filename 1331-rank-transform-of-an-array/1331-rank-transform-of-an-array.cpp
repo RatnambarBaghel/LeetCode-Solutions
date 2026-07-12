25f459269ee39105d9c82d8fc72c1c5ad1fbd94b
RatnambarBaghel <ratnambarbaghel14@gmail.com>
2026-07-12 22:47:34 +0530

    Time: 75 ms (16.81%), Space: 50.9 MB (10.13%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
         int n = arr.size();
-        vector<int> sortArr = arr;
-        map<int,int> map;
-        sort(sortArr.begin(), sortArr.end());
+        set<int> s(arr.begin(), arr.end());
         int rank =1;
+        unordered_map<int,int> map;
         vector<int> ans(n);
-        for(int i=0;i<n;i++){
-            if(i > 0 && sortArr[i] > sortArr[i-1]){
-                rank +=1;
+        for(int x : s){
+            if(map.find(x) == map.end()){
+                map[x] = rank++;
             }
-            map[sortArr[i]] = rank;
         }
-        for(int i =0;i<n;i++){
+        for(int i=0;i<n;i++){
             ans[i] = map[arr[i]];
         }
         return ans;
